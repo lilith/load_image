42fd643214b6437e06f292c49b346e885388a90f
Kornel Lesiński <kornel@geekhood.net>
2017-03-13 12:01:08 +0000

    Explicit LE dependency

--- src/pixel_format.rs
+++ src/pixel_format.rs
@@ -30,22 +30,28 @@ macro_rules! pixel_format {
     };
 }
 
-// assumes LE CPU :(
 pixel_format!{RGB8, PixelFormat::RGB_8, ColorSpaceSignature::SigRgbData }
-pixel_format!{RGB16, PixelFormat::RGB_16, ColorSpaceSignature::SigRgbData }
 pixel_format!{RGBA8, PixelFormat::RGBA_8, ColorSpaceSignature::SigRgbData }
-pixel_format!{RGBA16, PixelFormat::RGBA_16, ColorSpaceSignature::SigRgbData }
 pixel_format!{lodepng::Grey<u8>, PixelFormat::GRAY_8, ColorSpaceSignature::SigGrayData }
-pixel_format!{lodepng::Grey<u16>, PixelFormat::GRAY_16, ColorSpaceSignature::SigGrayData }
 pixel_format!{lodepng::GreyAlpha<u8>, PixelFormat::GRAYA_8, ColorSpaceSignature::SigGrayData }
+#[cfg(target_endian = "little")]
+pixel_format!{RGB16, PixelFormat::RGB_16, ColorSpaceSignature::SigRgbData }
+#[cfg(target_endian = "little")]
+pixel_format!{RGBA16, PixelFormat::RGBA_16, ColorSpaceSignature::SigRgbData }
+#[cfg(target_endian = "little")]
+pixel_format!{lodepng::Grey<u16>, PixelFormat::GRAY_16, ColorSpaceSignature::SigGrayData }
+#[cfg(target_endian = "little")]
 pixel_format!{lodepng::GreyAlpha<u16>, PixelFormat::GRAYA_16, ColorSpaceSignature::SigGrayData }
 
-// assumes LE CPU :(
 pixel_conversion!{RGB8 => RGB16, RGB16}
-pixel_conversion!{RGB16 => RGB16, RGB16}
 pixel_conversion!{RGBA8 => RGBA16, RGB16}
-pixel_conversion!{RGBA16 => RGBA16, RGB16}
 pixel_conversion!{lodepng::Grey<u8> => lodepng::Grey<u16>, lodepng::Grey<u16>}
-pixel_conversion!{lodepng::Grey<u16> => lodepng::Grey<u16>, lodepng::Grey<u16>}
 pixel_conversion!{lodepng::GreyAlpha<u8> => lodepng::GreyAlpha<u16>, lodepng::Grey<u16>}
+#[cfg(target_endian = "little")]
+pixel_conversion!{RGB16 => RGB16, RGB16}
+#[cfg(target_endian = "little")]
+pixel_conversion!{RGBA16 => RGBA16, RGB16}
+#[cfg(target_endian = "little")]
+pixel_conversion!{lodepng::Grey<u16> => lodepng::Grey<u16>, lodepng::Grey<u16>}
+#[cfg(target_endian = "little")]
 pixel_conversion!{lodepng::GreyAlpha<u16> => lodepng::GreyAlpha<u16>, lodepng::Grey<u16>}
